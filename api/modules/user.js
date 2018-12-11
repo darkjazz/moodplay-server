6cdbeb3f3dfc57277f6b53eeb4553e89dcb1e96a
alo <alo@anrita.wkstn.qmul.ac.uk>
2018-12-11 11:36:14 +0000

    recalculated features + socket.io stuff

--- api/modules/user.js
+++ api/modules/user.js
@@ -2,10 +2,17 @@ var uuid = require('uuid/v4');
 var time = require('time');
 
 const base_uri = 'https://moodplay.github.io/party/';
-const time_mimit = 10;
-
+const time_limit = 10000;
+const max_bots = 5;
+const names = [
+  "atete", "kianda", "emotan", "ayaba", "gleti", "mawu", "kaikara", "aja", "oba",
+  "oshun", "ayao", "mamlambo", "olapa", "nambi", "manat", "nuha", "bagmasti", "saris",
+  "nane", "nar", "huba", "wala", "bila", "dilga", "ekhi", "tanit"
+];
+var bot_names = [];
+var bot_name;
 var parties = {
-  'global': { id: 'global', owner_id: 'global', uri: base_uri + 'global', users: { } }
+  'global': { id: 'global', owner_id: 'moodplay', uri: base_uri + 'global', users: { } }
 }
 
 var check_user = function(uaid) {
@@ -27,7 +34,7 @@ var add_party = function(user_id) {
     id: id,
     uri: uri,
     owner_id: user_id,
-    updated: (new time.Date()).toString(),
+    updated: Date.now(),
     users: { }
   };
   party['users'][user_id] = parties['global']['users'][user_id];
@@ -46,7 +53,7 @@ var add_user = function(party_id, uaid, name) {
       uaid: uaid,
       uri: uri,
       name: name,
-      updated: (new time.Date()).toString(),
+      updated: Date.now(),
       current_coords: { valence: 0, arousal: 0, date: "0" },
       history: [ ]
     };
@@ -61,13 +68,68 @@ var add_user_coordinates = function(party_id, user_id, valence, arousal) {
     user['history'].push(user['current_coords']);
   }
   user['current_coords'] = {
-    date: (new time.Date()).toString(),
+    date: Date.now(),
     valence: parseFloat(valence),
     arousal: parseFloat(arousal)
   };
   parties[party_id]['users'][user_id] = user;
   return parties[party_id];
 }
+
+var calculate_average_coordinates = function(party_id) {
+  var avg_coords = { valence: 0, arousal: 0 };
+  var active_users = get_active_users(party_id);
+  if (active_users.length == 0) {
+    var id = find_user_id_by_name('global', bot_name);
+    add_user_coordinates('global', id, (Math.random()-0.5)*2.0, (Math.random()-0.5)*2.0);
+    active_users = get_active_users(party_id);
+  }
+  Object.keys(parties[party_id].users).forEach(uaid => {
+    var user = parties[party_id].users[uaid];
+    if (Date.now() - user.current_coords.date < time_limit) {
+      avg_coords.valence += user.current_coords.valence;
+      avg_coords.arousal += user.current_coords.arousal;
+    }
+  });
+  avg_coords.valence /= active_users.length;
+  avg_coords.arousal /= active_users.length;
+  return { avg_coords: avg_coords, active_users: active_users };
+}
+
+var get_active_users = function(party_id) {
+  var active_users = [];
+  Object.keys(parties[party_id].users).forEach(uaid => {
+    var user = parties[party_id].users[uaid];
+    if (Date.now() - user.current_coords.date < time_limit) {
+      active_users.push(user);
+    }
+  });
+  return active_users;
+}
+
+var find_user_id_by_name = function(party_id, name) {
+  var id;
+  Object.keys(parties[party_id].users).forEach(user_id => {
+    var user = parties[party_id].users[user_id];
+    if (user.name == name) {
+      id = user.id;
+    }
+  });
+  return id;
+}
+
+var generate_name = function() {
+  var index = Math.floor((Math.random() * names.length)) - 1;
+  return names[index];
+}
+
+bot_name = generate_name();
+add_user('global', 'moodplay', bot_name);
+
+// Array.from({length: max_bots}, (i) => {
+//   bot_name[i] = generate_name();
+//   add_user('global', 'moodplay', bot_name[i]);
+// });
 
 module.exports.add_user_coordinates = function(party_id, user_id, valence, arousal, cb) {
   cb(add_user_coordinates(party_id, user_id, valence, arousal))
@@ -79,4 +141,16 @@ module.exports.add_user = function(party_id, uaid, name, cb) {
 
 module.exports.create_new_party = function(user_id, cb) {
   cb(add_party(user_id))
+}
+
+module.exports.calculate_average_coordinates = function(party_id, cb) {
+  cb(calculate_average_coordinates(party_id))
+}
+
+module.exports.get_active_users = function(party_id, cb) {
+  cb(get_active_users(party_id))
+}
+
+module.exports.find_user_id_by_name = function(party_id, name, cb) {
+  cb(find_user_id_by_name(party_id, name))
 }
--- moodplay-server.js
+++ moodplay-server.js
@@ -5,6 +5,7 @@ var bodyParser = require('body-parser');
 var front = require('./api/modules/front');
 var http = require('http');
 var socket = require('socket.io');
+var user = require('./api/modules/user');
 
 var app = module.exports = express();
 var server = http.Server(app);
@@ -24,10 +25,20 @@ app.get('/', function (req, res) {
   res.send(front.serve_front())
 });
 
-io.on('connection', function(socket){
-  console.log('a user connected');
-});
+setInterval(function() {
+    user.calculate_average_coordinates('global', function(coords) {
+      console.log(coords.avg_coords, coords.active_users.map(user => user.name))
+    })
+}, 1000);
 
 server.listen(port, function () {
   console.log('Moodplay server listening on port ' + port + '!')
+});
+
+io.on('connection', function(socket) {
+  socket.on("user_coordinates", function(coords) {
+    user.add_user_coordinates(coords.partyID, coords.id, coords.valence, coords.arousal, function(party) {
+      socket.emit("party_message", party)
+    })
+  })
 });
